5455090c789ff9d796763626e19d67399eb2b186
Jedrzej Krauze <jedrzej.krauze@gmail.com>
2016-12-01 00:14:47 +0100

    refactoring: removed machine head and changed machine to use interface tape

--- src/turingMachine/TuringMachine.java
+++ src/turingMachine/TuringMachine.java
@@ -1,28 +1,38 @@
 package turingMachine;
 
+import turingMachine.tape.Tape;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
+import turingMachine.tape.LinkedListTape;
 
-abstract class TuringMachine {
-    
+public class TuringMachine {
+
     public static final char EMPTY_CHAR = 'B';
+    public static final int END_STATE = -1;
 
-    List<Character> tape;
-    private MachineHead machineHead;
+    private Tape tape;
+    private int state;
     private ArrayList<Map<Character, Transition>> transitionFunction;
 
-    TuringMachine(ArrayList<Map<Character, Transition>> transitionFunction) {
+    public TuringMachine(ArrayList<Map<Character, Transition>> transitionFunction) {
         this.transitionFunction = transitionFunction;
     }
 
-    void execute(String word) throws WrongTransitionFunctionException {
-        this.tape = word.chars().mapToObj(c->(char) c).collect(Collectors.toList());
-        this.machineHead = new MachineHead(tape, 0, 0);
-        while(machineHead.getState() != -1) {
-//            System.out.println(String.valueOf(tape));
-            machineHead.move(this.transitionFunction.get(machineHead.getState()).get(machineHead.getCharacter()));
+    public void execute(String word) throws WrongTransitionFunctionException {
+        this.tape = new LinkedListTape(word);
+        this.state = 0;
+        while (state != END_STATE) {
+            Transition transition = this.transitionFunction.get(state).get(tape.read());
+            if (transition == null) {
+                throw new WrongTransitionFunctionException();
+            }
+            this.tape.write(transition.getSignToWrite());
+            this.tape.move(transition.getDirection());
+            this.state = transition.getStateToGo();
         }
+    }
+
+    public String printTape() {
+        return this.tape.print();
     }
 }
